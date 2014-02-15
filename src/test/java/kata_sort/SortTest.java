37e7d21962084002a234352ab658fe74a46fd9bc
Somkiat <somkiat.p@gmail.com>
2014-02-15 19:32:00 +0700

    Add test case  (2,1,3) => (1,2,3) that pass

--- src/test/java/kata_sort/SortTest.java
+++ src/test/java/kata_sort/SortTest.java
@@ -36,6 +36,11 @@ public class SortTest {
 		assertEquals(list(1,2,3), sort.sort(list(1,2,3)));
 	}
 	
+	@Test
+	public void listOf3ValueThatOrderedShouldReturnOrderedList2() throws Exception {
+		assertEquals(list(1,2,3), sort.sort(list(2,1,3)));
+	}
+	
 	
 	private List<Integer> list(int... integers) {
 		List<Integer> list = new ArrayList<Integer>();
