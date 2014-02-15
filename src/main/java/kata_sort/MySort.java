24ffe91f88c23e0dbba8cc07dc0f83a7b665f749
Somkiat <somkiat.p@gmail.com>
2014-02-15 19:06:56 +0700

    Delete code and back to case [2,1]=>[1,2]

--- src/main/java/kata_sort/MySort.java
+++ src/main/java/kata_sort/MySort.java
@@ -5,29 +5,7 @@ import java.util.List;
 public class MySort {
 
 	public List<Integer> sort(List<Integer> list) {
-		if (list.size() >= 2) {
-			swapAll(list, 0);
-			sort(list.subList(0, list.size()-1));
-		}
 		return list;
-	}
-	
-	private void swapAll(List<Integer> list, int startPosition) {
-		if(startPosition +1 > list.size()-1) return;
-		swap(list, startPosition, startPosition+1);
-		swapAll(list, startPosition+1);
-	}
-
-	private boolean isSwap(List<Integer> list, int startPosition, int finishPosition) {
-		return list.get(startPosition) > list.get(finishPosition);
-	}
-
-	private void swap(List<Integer> list, int startPosition, int finishPosition) {
-		if (isSwap(list, startPosition, finishPosition)) {
-			int temp = list.get(startPosition);
-			list.set(startPosition, list.get(finishPosition));
-			list.set(finishPosition, temp);
-		}
 	}
 
 }
--- src/test/java/kata_sort/SortTest.java
+++ src/test/java/kata_sort/SortTest.java
@@ -31,40 +31,6 @@ public class SortTest {
 		assertEquals(list(1,2), sort.sort(list(2,1)));
 	}
 	
-	@Test
-	public void listOf3ValueThatOrdersShouldReturnSameList() throws Exception {
-		assertEquals(list(1,2,3), sort.sort(list(1,2,3)));
-	}
-	
-	@Test
-	public void listOf3ValueThatNotOrderShouldReturnOrderList() throws Exception {
-		assertEquals(list(1,2,3), sort.sort(list(2,1,3)));
-	}
-	
-	@Test
-	public void listOf3ValueThatNotOrderShouldReturnOrderList2() throws Exception {
-		assertEquals(list(1,2,3), sort.sort(list(1,3,2)));
-	}
-	
-	@Test
-	public void listOf3ValueThatNotOrderShouldReturnOrderList3() throws Exception {
-		assertEquals(list(1,2,3), sort.sort(list(3,1,2)));
-	}
-	
-	@Test
-	public void listOf4ValueThatOrdersShouldReturnSameList() throws Exception {
-		assertEquals(list(1,2,3,4), sort.sort(list(1,2,3,4)));
-	}
-	
-	@Test
-	public void listOf4ValueThatNotOrderShouldReturnOrderList() throws Exception {
-		assertEquals(list(1,2,3,4), sort.sort(list(4,3,2,1)));
-	}
-	
-	@Test
-	public void listOf9ValueThatNotOrderShouldReturnOrderList() throws Exception {
-		assertEquals(list(1,2,3,4,5,6,7,8,9), sort.sort(list(9,8,7,6,5,4,3,2,1)));
-	}
 	
 	private List<Integer> list(int... integers) {
 		List<Integer> list = new ArrayList<Integer>();
