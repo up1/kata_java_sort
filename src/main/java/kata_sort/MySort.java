f26080b5bd243fa7cbb22896ded1ab0b163bd080
Somkiat <somkiat.p@gmail.com>
2014-02-15 19:12:45 +0700

    Coding production code to pass test without assignment, i create new list to return

--- src/main/java/kata_sort/MySort.java
+++ src/main/java/kata_sort/MySort.java
@@ -1,11 +1,28 @@
 package kata_sort;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MySort {
 
 	public List<Integer> sort(List<Integer> list) {
+		if (list.size() == 2) {
+			int firstNumber = list.get(0);
+			int secondNumber = list.get(1);
+			if (firstNumber > secondNumber) {
+				return list(secondNumber, firstNumber);
+			} else {
+				return list(firstNumber, secondNumber);
+			}
+		}
 		return list;
 	}
 
+	private List<Integer> list(int... integers) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int integer : integers) {
+			list.add(integer);
+		}
+		return list;
+	}
 }
