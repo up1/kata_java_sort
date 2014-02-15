e97a99cebec61c33a23e774a033a588fc4017b57
Somkiat <somkiat.p@gmail.com>
2014-02-15 16:39:50 +0700

    Refactoring to extract to isSwap method before swap

--- src/main/java/kata_sort/MySort.java
+++ src/main/java/kata_sort/MySort.java
@@ -10,14 +10,18 @@ public class MySort {
 				swap(list, 0, 1);
 			}
 		} else if(list.size() == 3) {
-			if (list.get(0) > list.get(1)) {
+			if (isSwap(list,0,1)) {
 				swap(list, 0, 1);
 			}
-			if (list.get(1) > list.get(2)) {
+			if (isSwap(list,1,2)) {
 				swap(list, 1, 2);
 			}
 		}
 		return list;
+	}
+	
+	private boolean isSwap(List<Integer> list, int startPosition, int finishPosition) {
+		return list.get(startPosition) > list.get(finishPosition);
 	}
 	
 	private void swap(List<Integer> list, int startPosition, int finishPosition) {
--- src/test/java/kata_sort/SortTest.java
+++ src/test/java/kata_sort/SortTest.java
@@ -46,10 +46,10 @@ public class SortTest {
 		assertEquals(list(1,2,3), sort.sort(list(1,3,2)));
 	}
 	
-//	@Test
-//	public void listOf3ValueThatNotOrderShouldReturnOrderList3() throws Exception {
-//		assertEquals(list(1,2,3), sort.sort(list(3,2,1)));
-//	}
+	@Test
+	public void listOf3ValueThatNotOrderShouldReturnOrderList3() throws Exception {
+		assertEquals(list(1,2,3), sort.sort(list(3,2,1)));
+	}
 	
 	private List<Integer> list(int... integers) {
 		List<Integer> list = new ArrayList<Integer>();
